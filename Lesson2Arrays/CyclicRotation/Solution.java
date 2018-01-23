25e0007864935652123ea209c7c5897c0980f93f
Jeferson Barros <im.jbalves@gmail.com>
2018-01-23 00:05:31 -0400

    Update Solution.java
    Task Score 100%

--- Lesson2Arrays/CyclicRotation/Solution.java
+++ Lesson2Arrays/CyclicRotation/Solution.java
@@ -1,5 +1,5 @@
 // you can also use imports, for example:
-import java.util.*;
+// import java.util.*;
 
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
@@ -8,18 +8,11 @@ class Solution {
     public int[] solution(int[] A, int K) {
         // write your code in Java SE 8
         if(A == null) return null;
-        int j = 0;
-        int[] B = Arrays.copyOf(A,A.length);
-        int arrayLength = A.length - K;
+        int[] B = new int[A.length];
+        int arrayLength = A.length;
         
-        for(int i = 0; i < K ; i++){
-            B[i] = A[arrayLength];
-            arrayLength++;
-        }
-        
-        for(int i = K; i < A.length ; i++){
-            B[i] = A[j];
-            j++;
+        for(int i = 0; i < A.length ; i++){
+            B[(i+K) % arrayLength] = A[i];
         }
         
         return B;
